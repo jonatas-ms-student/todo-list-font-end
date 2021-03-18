832918f55dc78bf6478f0868d0c865c4ee2f8d8f
Jonatas Melo Silva <jonatas891024@gmail.com>
2021-03-17 23:16:40 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <Contador />
-    </div>
+    <h1>Home</h1>
   )
 }
 
-const Contador = () => {
-
-  const [contador, setContador] = useState(0);
-  const adicionarContador = () => {
-    setContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>Adicionar</button>
-    </div>
-  )
-}
 export default Home;
